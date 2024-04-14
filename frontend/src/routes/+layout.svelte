8fae7d4c6e1f168700baaab3bcbdcb47274b2cc6
gandolh <29356778+gandolh@users.noreply.github.com>
2024-04-14 17:12:57 +0300

    added tailwind daisy ui and bg for home

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,53 +1,36 @@
 <script>
-	import Header from './Header.svelte';
 	import './styles.css';
+	import "tailwindcss/tailwind.css";
 </script>
 
 <div class="app">
-	<Header />
-
 	<main>
-		<slot />
-	</main>
+		<div class="card w-96 glass">
+			<div class="card-body">
+			  <slot />
+			</div>
+		  </div>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+			
+	</main>
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
+	.app{
+		background-color: #f2f2f2;
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position: center;
+		/* background-image: url('$lib/images/wraper_back.jpg'); */
+		background-image: url('$lib/images/cool-background.svg');
 	}
 
-	main {
-		flex: 1;
+	main{
+		width: 100dvw;
+		height: 100dvh;
 		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		padding: 12px;
 	}
 
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
 </style>
