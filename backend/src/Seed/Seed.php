48c235d24d2782154a85d7d7bf93a1156d0c535d
gandolh <29356778+gandolh@users.noreply.github.com>
2024-05-18 23:25:04 +0300

    fetch with auth

--- backend/src/Seed/Seed.php
+++ backend/src/Seed/Seed.php
@@ -56,6 +56,7 @@ $createUserTable = "CREATE TABLE IF NOT EXISTS User (
     address TEXT,
     sex INTEGER,
     CNP TEXT,
+    google_id TEXT,
     FOREIGN KEY (roleId) REFERENCES Role(id)
 )";
 
@@ -107,18 +108,18 @@ $db->exec($insertRoleStudent);
 
 
 // Inserting users with different roles
-$insertUser1 = "INSERT INTO User (email, roleId, name, surname, birthDate, country, state, city, address, sex, CNP) VALUES ('user1@example.com', 1, 'John', 'Doe', '1990-01-01', 'USA', 'California', 'Los Angeles', '123 Main St', 1, '1234567890123')";
-$insertUser2 = "INSERT INTO User (email, roleId, name, surname, birthDate, country, state, city, address, sex, CNP) VALUES ('user2@example.com', 2, 'Jane', 'Smith', '1995-05-15', 'UK', 'England', 'London', '456 Oak St', 0, '9876543210987')";
-$insertUser3 = "INSERT INTO User (email, roleId, name, surname, birthDate, country, state, city, address, sex, CNP) VALUES ('user3@example.com', 3, 'Alice', 'Johnson', '1988-09-20', 'Canada', 'Ontario', 'Toronto', '789 Elm St', 1, '4567890123456')";
+$insertUser1 = "INSERT INTO User (id, email, roleId, name, surname, birthDate, country, state, city, address, sex, CNP) VALUES(1, 'ionut.blidaru02@e-uvt.ro', 3, 'Ionut', 'Blidaru', '1990-01-01T00:00:00+01:00', 'Romania', 'Timis', 'Timisoara', 'Oak Street 432', 0, '202035675542')";
+$insertUser2 = "INSERT INTO User (id, email, roleId, name, surname, birthDate, country, state, city, address, sex, CNP) VALUES(2, 'cristian.gusatu02@e-uvt.ro', 3, 'Cristian', 'Gusatu', '1990-01-01T00:00:00+01:00', 'Romania', 'Timis', 'Timisoara', 'Daliei 2', 0, '074242422222')";
+
 
 $db->exec($insertUser1);
 $db->exec($insertUser2);
-$db->exec($insertUser3);
 
 // Inserting discipline types
 $insertDisciplineType1 = "INSERT INTO DisciplineType (type) VALUES ('Optionala')";
 $insertDisciplineType2 = "INSERT INTO DisciplineType (type) VALUES ('Facultativa')";
 $insertDisciplineType3 = "INSERT INTO DisciplineType (type) VALUES ('Obligatorie')";
+
 
 $db->exec($insertDisciplineType1);
 $db->exec($insertDisciplineType2);
@@ -128,52 +129,63 @@ $db->exec($insertDisciplineType3);
 $insertDiscipline1 = "INSERT INTO Discipline (name, idDiscipline, credits) VALUES ('Introduction to Computer Science', 1, 3)";
 $insertDiscipline2 = "INSERT INTO Discipline (name, idDiscipline, credits) VALUES ('Data Structures and Algorithms', 2, 4)";
 $insertDiscipline3 = "INSERT INTO Discipline (name, idDiscipline, credits) VALUES ('Database Management Systems', 3, 4)";
+$insertDiscipline4 = "INSERT INTO Discipline (name, idDiscipline, credits) VALUES ('Software Engineering', 4, 5)";
+$insertDiscipline5 = "INSERT INTO Discipline (name, idDiscipline, credits) VALUES ('Computer Networks', 5, 4)";
+
+
 
 $db->exec($insertDiscipline1);
 $db->exec($insertDiscipline2);
 $db->exec($insertDiscipline3);
+$db->exec($insertDiscipline4);
+$db->exec($insertDiscipline5);
 
 
 // Inserting exam entries
 $insertExam1 = "INSERT INTO Exam (idDiscipline, examDate) VALUES (1, '2024-02-10')";
 $insertExam2 = "INSERT INTO Exam (idDiscipline, examDate) VALUES (2, '2024-02-15')";
 $insertExam3 = "INSERT INTO Exam (idDiscipline, examDate) VALUES (3, '2024-02-20')";
+$insertExam4 = "INSERT INTO Exam (idDiscipline, examDate) VALUES (4, '2024-02-25')";
+$insertExam5 = "INSERT INTO Exam (idDiscipline, examDate) VALUES (5, '2024-02-30')";
+
 
 $db->exec($insertExam1);
 $db->exec($insertExam2);
 $db->exec($insertExam3);
+$db->exec($insertExam4);
+$db->exec($insertExam5);
 
 // Inserting grade entries
-$insertGrade1 = "INSERT INTO Grade (idUser, idExam, value) VALUES (1, 1, 7)";
-$insertGrade2 = "INSERT INTO Grade (idUser, idExam, value) VALUES (2, 1, 6)";
-$insertGrade3 = "INSERT INTO Grade (idUser, idExam, value) VALUES (3, 1, 8)";
-$insertGrade4 = "INSERT INTO Grade (idUser, idExam, value) VALUES (1, 2, 9)";
-$insertGrade5 = "INSERT INTO Grade (idUser, idExam, value) VALUES (2, 2, 8)";
-$insertGrade6 = "INSERT INTO Grade (idUser, idExam, value) VALUES (3, 2, 7)";
-$insertGrade7 = "INSERT INTO Grade (idUser, idExam, value) VALUES (1, 3, 6)";
-$insertGrade8 = "INSERT INTO Grade (idUser, idExam, value) VALUES (2, 3, 7)";
-$insertGrade9 = "INSERT INTO Grade (idUser, idExam, value) VALUES (3, 3, 8)";
-
-$db->exec($insertGrade1);
-$db->exec($insertGrade2);
-$db->exec($insertGrade3);
-$db->exec($insertGrade4);
-$db->exec($insertGrade5);
-$db->exec($insertGrade6);
-$db->exec($insertGrade7);
-$db->exec($insertGrade8);
-$db->exec($insertGrade9);
+$insertGrade11 = "INSERT INTO Grade (idUser, idExam, value) VALUES (1, 1, 7)";
+$insertGrade12 = "INSERT INTO Grade (idUser, idExam, value) VALUES (2, 1, 6)";
+$insertGrade21 = "INSERT INTO Grade (idUser, idExam, value) VALUES (1, 2, 5)";
+$insertGrade22 = "INSERT INTO Grade (idUser, idExam, value) VALUES (2, 2, 6)";
+$insertGrade31 = "INSERT INTO Grade (idUser, idExam, value) VALUES (1, 3, 7)";
+$insertGrade32 = "INSERT INTO Grade (idUser, idExam, value) VALUES (2, 3, 8)";
+$insertGrade41 = "INSERT INTO Grade (idUser, idExam, value) VALUES (1, 4, 9)";
+$insertGrade42 = "INSERT INTO Grade (idUser, idExam, value) VALUES (2, 4, 10)";
+$insertGrade51 = "INSERT INTO Grade (idUser, idExam, value) VALUES (1, 5, 7)";
+$insertGrade52 = "INSERT INTO Grade (idUser, idExam, value) VALUES (2, 5, 8)";
+$db->exec($insertGrade11);
+$db->exec($insertGrade12);
+$db->exec($insertGrade21);
+$db->exec($insertGrade22);
+$db->exec($insertGrade31);
+$db->exec($insertGrade32);
+$db->exec($insertGrade41);
+$db->exec($insertGrade42);
+$db->exec($insertGrade51);
+$db->exec($insertGrade52);
 
 // Inserting user-discipline relationships
 $insertUserDiscipline1 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (1, 1)";
 $insertUserDiscipline2 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (2, 1)";
-$insertUserDiscipline3 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (3, 1)";
-$insertUserDiscipline4 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (1, 2)";
-$insertUserDiscipline5 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (2, 2)";
-$insertUserDiscipline6 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (3, 2)";
-$insertUserDiscipline7 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (1, 3)";
-$insertUserDiscipline8 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (2, 3)";
-$insertUserDiscipline9 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (3, 3)";
+$insertUserDiscipline3 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (1, 2)";
+$insertUserDiscipline4 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (2, 2)";
+$insertUserDiscipline5 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (1, 3)";
+$insertUserDiscipline6 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (2, 3)";
+$insertUserDiscipline7 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (1, 4)";
+$insertUserDiscipline8 = "INSERT INTO users_has_disciplines (id_user, id_discipline) VALUES (2, 4)";
 
 $db->exec($insertUserDiscipline1);
 $db->exec($insertUserDiscipline2);
@@ -183,7 +195,6 @@ $db->exec($insertUserDiscipline5);
 $db->exec($insertUserDiscipline6);
 $db->exec($insertUserDiscipline7);
 $db->exec($insertUserDiscipline8);
-$db->exec($insertUserDiscipline9);
 
 
 
